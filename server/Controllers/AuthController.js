175c64433c18d24aa45226055e44072ae8d3d7a1
rujal2004 <141760256+rujal2004@users.noreply.github.com>
2026-02-28 21:05:23 +0530

    commit

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,73 +1,104 @@
-const { StatusCodes } = require('http-status-codes');
-const UserSchema = require('../modals/UserSchema');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { StatusCodes } = require("http-status-codes");
+const UserSchema = require("../modals/UserSchema");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
+/* =========================
+   REGISTER USER
+========================= */
 const register = async (req, res) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
-        const profileImage = req.file;
-        if (!profileImage) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "No file Upload" });
-        }
-        const profileImagePath = profileImage.path;
-        const existingUser = await UserSchema.findOne({ email });
-        if (existingUser) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "User Already exist!" });
-        }
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const profileImage = req.file;
 
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-
-        const newUser = new UserSchema({
-            firstName,
-            lastName,
-            email,
-            password: hashPassword,
-            profileImagePath,
-        });
-        await newUser.save();
-        res.status(StatusCodes.CREATED).json({
-            success: true,
-            msg: "User Register Succesfully!",
-            user: newUser
-        });
-    } catch (error) {
-        console.log(error)
+    if (!profileImage) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "No profile image uploaded" });
     }
-}
 
+    const existingUser = await UserSchema.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "User already exists!" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new UserSchema({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      profileImagePath: profileImage.path,
+    });
+
+    await newUser.save();
+
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      msg: "User registered successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+/* =========================
+   LOGIN USER
+========================= */
 const login = async (req, res) => {
-    try {
-        /* Take the infomation from the form */
-        const { email, password } = req.body
+  try {
+    const { email, password } = req.body;
 
-        /* Check if user exists */
-        console.log(email, password)
-        const user = await UserSchema.findOne({ email });
-        if (!user) {
-            return res.status(StatusCodes.CONFLICT).json({ success: false, msg: "User doesn't exist!" });
-        }
-
-        /* Compare the password with the hashed password */
-        const isMatch = await bcrypt.compare(password, user.password)
-        if (!isMatch) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ success: false,msg: "Invalid Credentials!" })
-        }
-
-        /* Generate JWT token */
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECERET)
-        delete user.password // data decode karke password destructurre kar liya hai
-
-        res.status(StatusCodes.OK).json({success: true, token, user })
-
-    } catch (err) {
-        console.log(err)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.message })
+    // Check if user exists
+    const user = await UserSchema.findOne({ email });
+    if (!user) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "User does not exist!" });
     }
-}
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, msg: "Invalid credentials!" });
+    }
+
+    // Generate JWT Token (FIXED SPELLING)
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    // Remove password from response
+    const { password: _, ...userWithoutPassword } = user._doc;
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      token,
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
 module.exports = {
-    register, login
-}
+  register,
+  login,
+};
